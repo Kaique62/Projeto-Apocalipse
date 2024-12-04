76191e42ca7cfff9a30ff9f3b3c3b61029509a45
vidigalbz <165282131+vidigalbz@users.noreply.github.com>
2024-12-04 17:24:27 -0300

    correção na alternancia de tela de cadastro / login

--- Cliente/Paginas/Login/script.js
+++ Cliente/Paginas/Login/script.js
@@ -8,71 +8,6 @@ function resizeTriangle() {
 }
 
 addEventListener("resize", resizeTriangle);
-
-function cadastrar(){
-    let informationID = ["emailCadastro", "passwordCadastro", "nameCadastro", "nascimentoCadastro", "generoCadastro", "telefoneCadastro"]
-    let dicionario = {}
-    for (let i = 0; i < informationID.length; i++){
-        if (document.getElementById(informationID[i]).value.trim() == "");
-        console.log("Preencha Todos os Campos!");
-        dicionario[informationID[i]] = document.getElementById(informationID[i]).value;
-    }
-}
-
-function login(){
-    let user = document.getElementById("userLogin").value;
-    let senha = document.getElementById("passwordLogin").value;
- 
-    if (user.trim() == "" | senha.trim() == ""){
-        console.log("Preencha Todos os Campos!");
-    }
-    else {
-        console.log(`Usuário: ${user}\n` 
-            + `Senha: ${senha}`)
-    }
-}
-
-function cadastro() {
-    let name = document.getElementById("nameCadastro").value;
-
-    let sexo;
-    if (document.getElementById("sexoMascCadastro").checked) {
-        sexo = 'Masculino'
-    } 
-    else if (document.getElementById("sexoFemCadastro").checked) {
-        sexo = 'Feminino'
-    } 
-    else if (document.getElementById("sexoOutroCadastro").checked) {
-        sexo = 'Outro'
-    }
-
-    let dataNasc = document.getElementById("nascimentoCadastro").value;
-
-    let telefone = document.getElementById("telefoneCadastro").value;
-
-    let emailRegister = document.getElementById("emailCadastro").value;
-
-    let senhaCadastro;
-    if (document.getElementById("passwordCadastro").value != document.getElementById("confirmPasswordCadastro").value) {
-        senhaCadastro = 'As senhas não coinscidem'
-    } else {
-        senhaCadastro = document.getElementById("passwordCadastro").value
-    }
-
-    if (name.trim() == "" | sexo.trim() == "" | dataNasc.trim() == "" | telefone.trim() == "" | emailRegister.trim() == "" | senhaCadastro.trim() == ""){
-        console.log("Preencha Todos os Campos!");
-    }
-    else {
-        console.log(`Nome: ${name}\n` 
-            + `Sexo: ${sexo}\n`
-            + `Data de Nascimento: ${dataNasc}\n`
-            + `Telefone: ${telefone}\n`
-            + `Email: ${emailRegister}\n`
-            + `Senha: ${senhaCadastro}`)
-    }
-}
-
-var usuariosCadastrados = {};
  
 function cadastrar(){
     let informationID = ["emailCadastro", "passwordCadastro", "nameCadastro", "nascimentoCadastro", "generoCadastro", "telefoneCadastro"]
@@ -82,6 +17,7 @@ function cadastrar(){
         console.log("Preencha Todos os Campos!");
         dicionario[informationID[i]] = document.getElementById(informationID[i]).value;
     }
+    console.log(dicionario);
 }
  
 function login(){
@@ -98,8 +34,8 @@ function login(){
 }
  
 function changeMode(){
-    document.getElementById("cadastrar").style.display = document.getElementById("divCadastro").style.display == "none" ? "block": "none"
-    document.getElementById("login").style.display = document.getElementById("divLogin").style.display == "none" ? "block": "none"
+    document.getElementById("cadastrar").style.display = document.getElementById("cadastrar").style.display == "flex" ? "none": "flex"
+    document.getElementById("login").style.display = document.getElementById("login").style.display == "none" ? "flex": "none";
 }
 
 function PasswordVisibility(){
