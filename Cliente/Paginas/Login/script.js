66fa7da52f57ed31dd01b442ee04321066bf061b
vidigalbz <165282131+vidigalbz@users.noreply.github.com>
2024-12-04 17:05:59 -0300

    triangulo!!!1!1!1!11!11
    dinamico btw

--- Cliente/Paginas/Login/script.js
+++ Cliente/Paginas/Login/script.js
@@ -1,5 +1,14 @@
 var usuariosCadastrados = {};
 
+var triangleImage = document.getElementById("triangle");
+
+
+function resizeTriangle() {
+    triangleImage.style.backgroundPosition = `-${0 + (1920 - window.innerWidth)}px`;
+}
+
+addEventListener("resize", resizeTriangle);
+
 function cadastrar(){
     let informationID = ["emailCadastro", "passwordCadastro", "nameCadastro", "nascimentoCadastro", "generoCadastro", "telefoneCadastro"]
     let dicionario = {}
@@ -63,12 +72,36 @@ function cadastro() {
     }
 }
 
-function cadastrar() {
-    document.getElementById('login').style.display = 'none';
-    document.getElementById("cadastrar").style.display = 'flex';
+var usuariosCadastrados = {};
+ 
+function cadastrar(){
+    let informationID = ["emailCadastro", "passwordCadastro", "nameCadastro", "nascimentoCadastro", "generoCadastro", "telefoneCadastro"]
+    let dicionario = {}
+    for (let i = 0; i < informationID.length; i++){
+        if (document.getElementById(informationID[i]).value.trim() == "");
+        console.log("Preencha Todos os Campos!");
+        dicionario[informationID[i]] = document.getElementById(informationID[i]).value;
+    }
+}
+ 
+function login(){
+    let user = document.getElementById("userLogin").value;
+    let senha = document.getElementById("passwordLogin").value;
+ 
+    if (user.trim() == "" | senha.trim() == ""){
+        console.log("Preencha Todos os Campos!");
+    }
+    else {
+        console.log(`Usuário: ${user}\n`
+            + `Senha: ${senha}`);
+    }
+}
+ 
+function changeMode(){
+    document.getElementById("cadastrar").style.display = document.getElementById("divCadastro").style.display == "none" ? "block": "none"
+    document.getElementById("login").style.display = document.getElementById("divLogin").style.display == "none" ? "block": "none"
 }
 
-function voltar() {
-    document.getElementById('login').style.display = 'flex';
-    document.getElementById("cadastrar").style.display = 'none';
+function PasswordVisibility(){
+
 }
