f507b46aeebd1d76c4a6bea5106a9b56b22c353f
Piterqle <pedrohenriquemendesmartins5@gmail.com>
2024-12-09 17:20:29 -0300

    Start conection MySql

---
+++ Cliente/Paginas/Agendamento/back-back-back-end.js
@@ -0,0 +1,8 @@
+const db = require('./db');
+
+function botar(){
+    a = document.getElementById("Nome");
+    b = document.getElementById("Idade");
+    c = document.getElementById("UF");
+    db.UpInsert(`INSERT INTO clientes (Nome, Idade, UF) VALUES('${a}', '${b}', '${c}')`)
+}
---
+++ Cliente/Paginas/Agendamento/main.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const db = require('./db');
+const path = require('path')
+const app = express();
+
+
+app.get('/clientes', async (req, res) => {  
+    try {
+        const results = await db.selectCustomers();
+        res.json(results);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro ao buscar clientes.');
+    }
+});
+
+app.use(express.static("Agendamento"))
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000.');
+});
